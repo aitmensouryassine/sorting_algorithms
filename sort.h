b5f3eeff0245d6ecde6484debb3df36e39161d4f
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-20 14:14:31 -0700

    Update sort.h

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
