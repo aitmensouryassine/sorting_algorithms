45f18d33e3099dae941ffcd11ebf0e2e4353cdf8
HAJAR <elabdellaoui.hajar@gmail.com>
2023-07-21 23:15:28 +0000

    update counting algo

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,22 +7,25 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int k = array[0], i;
+	size_t i;
+	int k;
 	int *counter, *sorted;
 
-	for (i = 1; i < (int)size; i++)
+	if (size < 2 || !array)
+		return;
+	for (i = 1, k = array[0]; i < size; i++)
 		if (array[i] > k)
 			k = array[i];
 
-	counter = malloc(sizeof(int) * (k + 1));
+	counter = malloc(sizeof(size_t) * (k + 1));
 	if (!counter)
 		return;
-	for (i = 0; i < (k + 1); i++)
+	for (i = 0; (int)i < k + 1; i++)
 		counter[i] = 0;
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 		counter[array[i]] += 1;
-	for (i = 0; i < k; i++)
-		counter[i + 1] += counter[i];
+	for (i = 1; (int)i <= k; i++)
+		counter[i] += counter[i - 1];
 	print_array(counter, k + 1);
 	sorted = malloc(sizeof(int) * (size));
 	if (!sorted)
@@ -30,12 +33,12 @@ void counting_sort(int *array, size_t size)
 		free(counter);
 		return;
 	}
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sorted[counter[array[i] - 1]] = array[i];
 		counter[array[i]] -= 1;
 	}
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = sorted[i];
 	free(sorted);
 	free(counter);
