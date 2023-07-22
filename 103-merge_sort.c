7a33bf237fd6932cf199906152777da980f59dfa
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-22 16:06:27 +0000

    Update merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,70 +3,67 @@
 /**
  *
  */
-void merge(int *arr, size_t start, size_t middle, size_t end, int *array)
+void print_array_interval(char *msg, int *array, size_t start, size_t end)
 {
-	size_t l, r, i;
-
-	printf("Merging...\n");
-
-	printf("[left]: ");
-	i = start;
-	while (array && i < middle)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-
-	printf("[right]: ");
-	i = middle;
-	while (array && i < end)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-
-	l = start, r = middle;
-	while (l < middle && r < end)
-		if (array[l] < array[r])
-			arr[i++] = array[l++];
-		else
-			arr[i++] = array[r++];
-
-	while (l < middle)
-		arr[i++] = array[l++];
-
-	while (r < end)
-		arr[i++] = array[r++];
-
-	printf("[Done]: ");
-	print_array(arr, end);
+	int i = 0;
+	printf("[%s]: ", msg);
+        while (array && start < end)
+        {
+                if (i++ > 0)
+                        printf(", ");
+                printf("%d", array[start]);
+                ++start;
+        }
+        printf("\n");
 }
 
 /**
  *
  */
-int *merge_sort_rec(int *array, size_t start, size_t end, int *arr)
+void merge(int *arr, size_t start, size_t middle, size_t end, int *array)
+{
+	size_t l, r, i;
+
+	printf("Merging...\n");
+	print_array_interval("left", array, start, middle);
+	print_array_interval("right", array, middle, end);
+
+	l = start, r = middle + 1, i = start;
+	while (l <= middle && r <= end)
+		if (array[l] < array[r])
+			arr[i++] = array[l++];
+		else
+			arr[i++] = array[r++];
+
+	while (l <= middle)
+		arr[i++] = array[l++];
+
+	while (r <= end)
+		arr[i++] = array[r++];
+
+	for (i = start; i < end + 1; i++)
+		array[i] = arr[i];
+
+	print_array_interval("Done", array, start, end);
+}
+
+/**
+ *
+ */
+void merge_sort_rec(int *array, size_t start, size_t end, int *arr)
 {
         size_t middle;
 
         /* base */
-        if ((end - start) == 1)
-                return (array);
+        if (end - start <= 1)
+		return;
 
-        middle = (start + end) / 2;
+	middle = (start + end + 1) / 2;
 
-        merge_sort_rec(array, start, middle - 1, arr);
-	merge_sort_rec(array, middle, end, arr);
+	merge_sort_rec(array, start, middle, arr);
+	merge_sort_rec(array, middle + 1, end, arr);
 
-        merge(arr, start, middle, end, array);
-
-	return (array);
+	merge(arr, start, middle, end, array);
 }
 
 /**
@@ -75,13 +72,10 @@ int *merge_sort_rec(int *array, size_t start, size_t end, int *arr)
 void merge_sort(int *array, size_t size)
 {
 	int *arr;
-	size_t i;
 
 	arr = malloc(sizeof(int) * size);
-	for (i = 0; i < size; i++)
-		arr[i] = array[i];
 
-	merge_sort_rec(array, 0, size, arr);
+	merge_sort_rec(array, 0, size - 1, arr);
 
 	free(arr);
 }
