20a7a48b483d2a21344e2c416ec087af8af64d0f
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-23 17:21:40 +0000

    Fix

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-
 /**
  * hoare_partition - sorts pivot
  * @array: The array to sort
@@ -12,24 +11,28 @@
 size_t hoare_partition(int *array, size_t start, size_t end, size_t size)
 {
 	int pivot = array[end], tmp;
-	size_t i = end, j = start;
+	size_t i = end + 1, j = start - 1;
 
 	while (1)
 	{
-		while (array[i] > pivot)
+		do {
 			i--;
+		} while (array[i] > pivot);
 
-		while (array[j] < pivot)
+		do {
 			j++;
+		} while (array[j] < pivot);
 
-		if (j >= i)
+		if (i < j)
 			return (i);
 
-		tmp = array[j];
-		array[j] = array[i];
-		array[i] = tmp;
-
-		print_array(array, size);
+		if (array[j] > array[i])
+		{
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
 	}
 }
 
@@ -45,12 +48,11 @@ void quick_sort_rec(int *array, int start, int end, size_t size)
 	size_t idx_part;
 
 	/* base */
-	if (end < start)
+	if (start >= end)
 		return;
 
 	idx_part = hoare_partition(array, start, end, size);
-
-	quick_sort_rec(array, start, idx_part - 1, size);
+	quick_sort_rec(array, start, idx_part, size);
 	quick_sort_rec(array, idx_part + 1, end, size);
 }
 
